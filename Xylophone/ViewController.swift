e0e79f7c8cbabc443c0f7267ccd8e932fc98765e
jafz <jafz.dev@gmail.com>
2020-01-08 22:19:50 -0500

    initial commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,28 @@
+//  Xylophone
+
+//
+import UIKit
+import AVFoundation//Audio visual foundation library
+
+class ViewController: UIViewController {
+    
+    //creating a new AVAudioPlayer
+    var player = AVAudioPlayer()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound(soundName: sender.currentTitle!)
+    }
+    
+    func playSound(soundName: String){
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
+}
+
